d665263bdac8352ad1e4934db828ab7370d3ec5b
Daniel <dfdazac@gmail.com>
2019-01-12 11:22:59 +0100

    Clean up utils

--- utils.py
+++ utils.py
@@ -1,124 +1,7 @@
 import numpy as np
 import scipy.sparse as sp
-from sklearn.metrics import roc_auc_score, average_precision_score
 import torch
 
-# Source: https://github.com/tkipf/gae
-def sparse_to_tuple(sparse_mx):
-    if not sp.isspmatrix_coo(sparse_mx):
-        sparse_mx = sparse_mx.tocoo()
-    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
-    values = sparse_mx.data
-    shape = sparse_mx.shape
-    return coords, values, shape
-
-def mask_test_edges(adj):
-    # Function to build test set with 10% positive links
-    # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
-    # TODO: Clean up.
-
-    # Remove diagonal elements
-    adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
-    adj.eliminate_zeros()
-    # Check that diag is zero:
-    assert np.diag(adj.todense()).sum() == 0
-
-    adj_triu = sp.triu(adj)
-    adj_tuple = sparse_to_tuple(adj_triu)
-    edges = adj_tuple[0]
-    edges_all = sparse_to_tuple(adj)[0]
-    num_test = int(np.floor(edges.shape[0] / 10.))
-    num_val = int(np.floor(edges.shape[0] / 20.))
-
-    all_edge_idx = np.arange(edges.shape[0])
-    np.random.shuffle(all_edge_idx)
-    val_edge_idx = all_edge_idx[:num_val]
-    test_edge_idx = all_edge_idx[num_val:(num_val + num_test)]
-    test_edges = edges[test_edge_idx]
-    val_edges = edges[val_edge_idx]
-    train_edges = np.delete(edges, np.hstack([test_edge_idx, val_edge_idx]), axis=0)
-
-    def ismember(a, b, tol=5):
-        rows_close = np.all(np.round(a - b[:, None], tol) == 0, axis=-1)
-        return np.any(rows_close)
-
-    test_edges_false = []
-    while len(test_edges_false) < len(test_edges):
-        idx_i = np.random.randint(0, adj.shape[0])
-        idx_j = np.random.randint(0, adj.shape[0])
-        if idx_i == idx_j:
-            continue
-        if ismember([idx_i, idx_j], edges_all):
-            continue
-        if test_edges_false:
-            if ismember([idx_j, idx_i], np.array(test_edges_false)):
-                continue
-            if ismember([idx_i, idx_j], np.array(test_edges_false)):
-                continue
-        test_edges_false.append([idx_i, idx_j])
-
-    val_edges_false = []
-    while len(val_edges_false) < len(val_edges):
-        idx_i = np.random.randint(0, adj.shape[0])
-        idx_j = np.random.randint(0, adj.shape[0])
-        if idx_i == idx_j:
-            continue
-        if ismember([idx_i, idx_j], train_edges):
-            continue
-        if ismember([idx_j, idx_i], train_edges):
-            continue
-        if ismember([idx_i, idx_j], val_edges):
-            continue
-        if ismember([idx_j, idx_i], val_edges):
-            continue
-        if val_edges_false:
-            if ismember([idx_j, idx_i], np.array(val_edges_false)):
-                continue
-            if ismember([idx_i, idx_j], np.array(val_edges_false)):
-                continue
-        val_edges_false.append([idx_i, idx_j])
-
-    assert ~ismember(test_edges_false, edges_all)
-    assert ~ismember(val_edges_false, edges_all)
-    assert ~ismember(val_edges, train_edges)
-    assert ~ismember(test_edges, train_edges)
-    assert ~ismember(val_edges, test_edges)
-
-    data = np.ones(train_edges.shape[0])
-
-    # Re-build adj matrix
-    adj_train = sp.csr_matrix((data, (train_edges[:, 0], train_edges[:, 1])), shape=adj.shape)
-    adj_train = adj_train + adj_train.T
-
-    # NOTE: these edge lists only contain single direction of edge!
-    return adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false
-
-def get_roc_scores(adj_pred, adj_orig, edges_pos=None, edges_neg=None):
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
-
-    if edges_pos is not None and edges_neg is not None:
-        # Evaluate performance on predicted adjacency matr
-        # Predict on test set of edges
-        preds = []
-        pos = []
-        for e in edges_pos:
-            preds.append(sigmoid(adj_pred[e[0], e[1]]))
-            pos.append(adj_orig[e[0], e[1]])
-
-        preds_neg = []
-        neg = []
-        for e in edges_neg:
-            preds_neg.append(sigmoid(adj_pred[e[0], e[1]]))
-            neg.append(adj_orig[e[0], e[1]])
-
-        preds_all = np.hstack([preds, preds_neg])
-        labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds))])
-
-    roc_score = roc_auc_score(labels_all, preds_all)
-    ap_score = average_precision_score(labels_all, preds_all)
-
-    return roc_score, ap_score
 
 def sample_zero_entries(mat):
     """A generator to obtain zero entries from a sparse matrix"""
@@ -134,6 +17,7 @@ def sample_zero_entries(mat):
             nonzero_or_sampled.add(t)
             nonzero_or_sampled.add((t[1], t[0]))
 
+
 def split_edges(edge_index):
     """Obtain positive and negative train/val/test edges for an *undirected*
     graph given m an edge index (as the one used in the
@@ -148,8 +32,7 @@ def split_edges(edge_index):
     adj.eliminate_zeros()
 
     adj_triu = sp.triu(adj)
-    adj_tuple = sparse_to_tuple(adj_triu)
-    edges = adj_tuple[0]
+    edges = np.array(adj_triu.nonzero()).T
     num_test = int(np.floor(edges.shape[0] / 10.))
     num_val = int(np.floor(edges.shape[0] / 20.))
 
@@ -182,6 +65,7 @@ def split_edges(edge_index):
 
     return positive_splits, negative_splits
 
+
 def add_reverse_edges(edges):
     """Add edges in the reverse direction to a tensor containing edges in
     one direction only.
@@ -191,6 +75,7 @@ def add_reverse_edges(edges):
     edges_inv = torch.stack((edges[1], edges[0]), dim=0)
     all_edges = torch.cat((edges, edges_inv), dim=1)
     return all_edges
+
 
 def adj_from_edge_index(edge_index):
     """Get a sparse symmetric adjacency matrix from an edge index (as the one
