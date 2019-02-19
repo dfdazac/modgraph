d1484d51b9ec3ad2c8fd0e3ce45a3968b6600c8d
Daniel <dfdazac@gmail.com>
2019-02-19 14:33:26 +0100

    Remove node2vec saved edges

--- models.py
+++ models.py
@@ -173,6 +173,7 @@ class Node2Vec(nn.Module):
 
         # Read embeddings and store in encoder
         emb_data = np.loadtxt(embs_path, skiprows=1)
+        os.remove(edges_path)
         os.remove(embs_path)
         embeddings = np.zeros([num_nodes, emb_data.shape[1] - 1])
         idx = emb_data[:, 0].astype(np.int)
