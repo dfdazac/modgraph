4a952ee131bd3b39bba52b5b42efe2a5eebc1b0b
Daniel <dfdazac@gmail.com>
2019-04-29 18:24:27 +0200

    SGE experimental setup

--- experiments.py
+++ experiments.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import time
+from datetime import datetime
 
 import torch
 from train import ex
@@ -20,33 +20,29 @@ if not args.log:
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Configuration template
-config = {'dataset_str': None,
-          'method': None,
-          'encoder_str': 'gcngauss',
+config = {'dataset_str': 'cora',
+          'method': 'sge',
+          'encoder_str': None,
           'hidden_dims': [256, 128],
-          'n_points': 4,
+          'n_points': None,
           'lr': None,
           'epochs': 200,
           'p_labeled': 0.1,
-          'n_exper': 20,
+          'n_exper': 1,
           'device': device,
-          'timestamp': str(int(time.time())),
+          'timestamp': str(int(datetime.now().timestamp())),
           'edge_score': 'inner'}
 
 # Values to be changed in experiments
-param_grid = {'method': ['graph2gauss', 'graph2vec'],
-              'dataset_str': ['cora', 'citeseer', 'pubmed', 'corafull',
-                              'coauthorcs', 'coauthorphys', 'amazoncomp',
-                              'amazonphoto'],
-              'lr': [1e-3, 1e-4, 5e-5]}
+param_grid = {'encoder_str': ['mlp', 'gcn', 'sgc'],
+              'n_points': [1, 4, 8, 16, 32, 64],
+              'lr': [1e-2, 1e-3, 1e-4]}
 
 grid = ParameterGrid(param_grid)
 
-for task in ['link_pred_experiments']:
-    for i, hparams in enumerate(grid):
-        print('Experiment configuration {:d}/{:d}'.format(i + 1, len(grid)))
-        config.update(hparams)
+for i, hparams in enumerate(grid):
+    print('Experiment configuration {:d}/{:d}'.format(i + 1, len(grid)))
+    config.update(hparams)
 
-        pp.pprint(config)
-        ex.run(command_name=task, config_updates=config)
+    pp.pprint(config)
+    ex.run(command_name='link_pred_experiments', config_updates=config)
